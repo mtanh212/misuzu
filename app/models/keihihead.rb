09f21aae69e715dd910638b8a25532fbce654181
Duc <duc@duc.local>
2016-04-21 17:39:18 +0700

    add dengon screen

--- app/models/keihihead.rb
+++ app/models/keihihead.rb
@@ -3,7 +3,8 @@ class Keihihead < ActiveRecord::Base
   self.primary_key = :申請番号
   
   has_many :keihibodys, foreign_key: :申請番号
-  
+  belongs_to :shainmaster, foreign_key: :社員番号
+
   accepts_nested_attributes_for :keihibodys,
                                 allow_destroy: true,
                                 reject_if: :all_blank
@@ -15,5 +16,7 @@ class Keihihead < ActiveRecord::Base
   }
 
   scope :current_member, ->(member) { where( 社員番号: member )}
+
+  delegate :氏名, to: :shainmaster, prefix: :shainmaster, allow_nil: true
 
 end 
