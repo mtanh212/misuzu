dfba7dc98642c0d01355e21f9e3452999d4557bc
tranduytung <tranduytung1994@gmail.com>
2016-07-22 11:55:58 +0700

    fix seed

--- db/seeds/10_tantosha.rb
+++ db/seeds/10_tantosha.rb
@@ -1,7 +1,8 @@
 User.delete_all
 User.reset_pk_sequence
 Shainmaster.all.each do |sha|
-    a = sha.社員番号
-    User.create! 担当者コード: a,担当者名称:'古川　知史',パスワード: a,再パスワード: a,
-      email: a+"@gmail.com", password: a, password_confirmation: a
+  a = sha.社員番号
+  User.create! 担当者コード: a,担当者名称:'古川　知史',パスワード: a,再パスワード: a,
+    email: a+"@gmail.com", password: a, password_confirmation: a
+
 end
