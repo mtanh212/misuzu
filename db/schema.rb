0af92eccc1a9f3ec84c45ddda51c0905a02ee967
MinhUchiha <nqminh27894@gmail.com>
2016-10-26 16:56:05 +0700

    fix setsubiyoyaku

--- db/schema.rb
+++ db/schema.rb
@@ -34,22 +34,6 @@ ActiveRecord::Schema.define(version: 20161025075428) do
 
   add_index "JOBマスタ", ["job番号"], name: "index_JOBマスタ_on_job番号", unique: true, using: :btree
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
-
   create_table "events", force: :cascade do |t|
     t.string   "社員番号"
     t.string   "開始"
@@ -386,10 +370,10 @@ ActiveRecord::Schema.define(version: 20161025075428) do
 
   create_table "設備予約", force: :cascade do |t|
     t.string   "設備コード"
-    t.string   "開始"
-    t.string   "終了"
     t.string   "予約者"
     t.string   "相手先"
+    t.string   "開始"
+    t.string   "終了"
     t.text     "用件"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
